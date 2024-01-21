988b1f464292ef7daa90a6e7b73214640933b2fe
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-21 14:40:48 +0100

    style(header): styling finished

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/components/header/header.component';
 import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, HeaderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
