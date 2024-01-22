6d8c14649d1403b95a2b80620217ad052840be3f
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-22 11:22:55 +0100

    style: line height of all headings adjusted

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent, HeaderComponent],
+  imports: [HeroComponent, HeaderComponent, AboutMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
