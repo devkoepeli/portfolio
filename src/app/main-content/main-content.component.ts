73742a3dce7eacfed727e494f5f5b3f8675ee25d
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-22 14:55:28 +0100

    style(skills): initial styling

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { HeroComponent } from './hero/hero.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeroComponent, HeaderComponent, AboutMeComponent],
+  imports: [
+    HeroComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    SkillSetComponent
+  ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
--- src/app/main-content/skill-set/skill-set.component.ts
+++ src/app/main-content/skill-set/skill-set.component.ts
@@ -1,12 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss'
 })
 export class SkillSetComponent {
-
+  skills = [
+    { name: 'Angular', image: './assets/img/angular.svg' },
+    { name: 'TypeScript', image: './assets/img/typescript.svg' },
+    { name: 'JavaScript', image: './assets/img/javascript.svg' },
+    { name: 'HTML', image: './assets/img/html.svg' },
+    { name: 'Scrum', image: './assets/img/scrum.svg' },
+    { name: 'Firebase', image: './assets/img/firebase.svg' },
+    { name: 'GIT', image: './assets/img/git.svg' },
+    { name: 'CSS', image: './assets/img/css.svg' },
+    { name: 'Rest-Api', image: './assets/img/api.svg' },
+    { name: 'Material', image: './assets/img/material.svg' }
+  ]
 }
