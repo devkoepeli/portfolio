b79c2b88802067037d34bfc24318546d3e6ec718
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-24 14:23:34 +0100

    feat: configure router to scroll to top

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { InMemoryScrollingOptions, InMemoryScrollingFeature, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes, inMemoryScrollingFeature)]
 };
