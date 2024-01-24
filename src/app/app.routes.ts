ffdfe6791d9e32429f9fcb7c3488925f8f8c20b5
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-24 09:40:23 +0100

    style(footer): initial styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
 import { MainContentComponent } from './main-content/main-content.component';
 
 export const routes: Routes = [
-    { path: '', component: MainContentComponent}
+    { path: '', component: MainContentComponent},
+    { path: 'imprint', component: ImprintComponent}
 ];
