b8b8fe00aa0bc5726b3ff3406afaca46cf205a25
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-24 14:26:15 +0100

    feat: delete scrolling anchor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,6 @@ import { routes } from './app.routes';
 
 const scrollConfig: InMemoryScrollingOptions = {
   scrollPositionRestoration: 'top',
-  anchorScrolling: 'enabled',
 };
 
 const inMemoryScrollingFeature: InMemoryScrollingFeature =
