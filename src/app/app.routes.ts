771e55b475133bde75710fdcc3f89265a63ab8c4
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-26 12:33:41 +0100

    add: privacy-policy + legal notice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { ImprintComponent } from './imprint/imprint.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    { path: '', component: MainContentComponent},
-    { path: 'imprint', component: ImprintComponent}
+    { path: '', component: MainContentComponent },
+    { path: 'legal-notice', component: LegalNoticeComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent }
 ];
--- src/app/main-content/contact/contact-form/contact-form.component.ts
+++ src/app/main-content/contact/contact-form/contact-form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-form',
@@ -8,7 +9,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   imports: [
     FormsModule,
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
