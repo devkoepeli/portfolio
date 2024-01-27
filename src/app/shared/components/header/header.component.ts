c5fad119e9a4603cbacec91bb3a09754df2ffe63
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-27 11:59:04 +0100

    feat(header): change logo on route change

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isMainRoute = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url == '/legal-notice' || event.url == '/privacy-policy') {
+          this.isMainRoute = false;
+        } else {
+          this.isMainRoute = true;
+        }
+      }
+    })
+  }
 }
