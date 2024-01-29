da650b8455d001e8c24b16caee78da5ae17f4ce4
devkoepeli <devrim_kopeli@hotmail.com>
2024-01-30 00:13:21 +0100

    chore: clean up

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -8,6 +8,23 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  clientHeight!: number;
+  isLegalNoticeRoute = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.clientHeight = window.innerHeight;
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url == '/legal-notice') {
+          this.isLegalNoticeRoute = true;
+        } else {
+          this.isLegalNoticeRoute = false;
+        }
+      }
+    })
+  }
 }
